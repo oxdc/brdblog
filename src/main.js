e747a7824b7c78339e3f7bffdfb57d7b94267a46
oxdc <frnet2016@gmail.com>
2018-10-11 09:58:30 +0800

    [update] reset CSS

--- src/main.js
+++ src/main.js
@@ -1,43 +1,51 @@
+// Load basic library - Vue
 import Vue from 'vue'
+
+// Load App
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Reset CSS
+import 'normalize.css/normalize.css'
+
+// Load themes
 import '@/assets/Themes/base.css'
 import '@/assets/Themes/default.css'
 
-import VueQuillEditor from 'vue-quill-editor'
+// Load Highlight.js
+import hljs from 'highlight.js'
+import 'highlight.js/styles/vs.css'
 
-// require styles
+// Load iView
+import iView from 'iview'
+import locale from 'iview/dist/locale/en-US'
+import 'iview/dist/styles/iview.css'
+
+// Load Katex
+import katex from 'katex'
+import 'katex/dist/katex.min.css'
+
+// Load Quill editor
+import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
 
-Vue.use(VueQuillEditor)
-
+// Load Vue Awesome Swiper
 import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-// require styles
 import 'swiper/dist/css/swiper.css'
 
-Vue.use(VueAwesomeSwiper)
-
-import hljs from 'highlight.js'
-import 'highlight.js/styles/vs.css'
-import 'katex/dist/katex.min.css'
-import katex from 'katex'
-
-import iView from 'iview'
-import 'iview/dist/styles/iview.css'
-import locale from 'iview/dist/locale/en-US'
-
+// Config Katex and Highlight
 window.katex = katex
 window.hljs = hljs
 
-Vue.use(iView, {
-  locale
-})
+// Config Vue modules
+Vue.use(VueQuillEditor)
+Vue.use(VueAwesomeSwiper)
+Vue.use(iView, { locale })
 
+// Config Vue App
 Vue.config.productionTip = false
 
 new Vue({
